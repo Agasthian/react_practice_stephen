e3d51db5ac13167afd3c0799f614c35695117df2
Agasthian <agasthian_r@hotmail.com>
2019-10-02 22:40:40 +0530

    Added Redux form to create stream component and connected it to redux state.Also done with validation of the fields

--- streamy/client/src/components/streams/StreamCreate.component.jsx
+++ streamy/client/src/components/streams/StreamCreate.component.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>
-      <h1>StreamCreate</h1>
-    </div>
-  );
+class StreamCreate extends Component {
+  renderError = ({ error, touched }) => {
+    if (error && touched) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderField = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValue) {
+    console.log(formValue);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form error'
+      >
+        <Field
+          name='title'
+          component={this.renderField}
+          label={'Enter the title of the stream'}
+        />
+        <Field
+          name='description'
+          component={this.renderField}
+          label={'Enter the description'}
+        />
+        <button className='ui primary button'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter the title';
+  }
+  if (!formValues.description) {
+    errors.description = 'Pls enter the description 😅 ';
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+  validate: validate
+})(StreamCreate);
