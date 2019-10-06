a0ab5f613dd7b70e66d80263ec0b9e38de41360d
Agasthian <agasthian_r@hotmail.com>
2019-10-06 21:37:27 +0530

    Compeleted the stream show component with redux state. Also impelemented the RTMP server to compelete the steraming part

--- streamy/client/src/App.js
+++ streamy/client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamList from './components/streams/StreamList.component';
 import StreamCreate from './components/streams/StreamCreate.component';
@@ -15,11 +15,13 @@ function App() {
       <Router history={history}>
         <div>
           <Header />
-          <Route path='/' exact component={StreamList} />
-          <Route path='/streams/new' component={StreamCreate} />
-          <Route path='/streams/edit/:id' component={StreamEdit} />
-          <Route path='/streams/delete/:id' component={StreamDelete} />
-          <Route path='/streams/show' component={StreamShow} />
+          <Switch>
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' component={StreamCreate} />
+            <Route path='/streams/edit/:id' component={StreamEdit} />
+            <Route path='/streams/delete/:id' component={StreamDelete} />
+            <Route path='/streams/:id' component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
